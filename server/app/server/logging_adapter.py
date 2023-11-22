ef384e9100f426398aed32140a38e90b7c9d6d94
corey <coreybatchler7@gmail.com>
2023-11-23 06:37:40 +1000

    Fix protocol logging default behaviour

--- server/app/server/logging_adapter.py
+++ server/app/server/logging_adapter.py
@@ -3,5 +3,5 @@ import logging
 class ProtocolLoggerAdapter(logging.LoggerAdapter):
     def process(self, msg, kwargs):
         protocol_ident = self.extra.get('ident', 'Unknown')
-        protostate_name = self.extra.get('protostate', 'None').__class__.__name__
+        protostate_name = self.extra.get('protostate', 'None')
         return f"[{protocol_ident}][{protostate_name}] {msg}", kwargs
--- server/app/server/protostates/protostate.py
+++ server/app/server/protostates/protostate.py
@@ -6,6 +6,9 @@ class ProtoState(ABC):
     def __init__(self, protocol: Protocol):
         self.proto = protocol
 
+    def __str__(self) -> str:
+        return self.__class__.__name__
+
     async def log_unregistered_packet(self, packet):
         self.proto.logger.warning(f"received {packet.DESCRIPTOR.name} packet in unregistered state")
         p = create_deny_packet("You cannot send that packet in this state")  # todo: better error message
